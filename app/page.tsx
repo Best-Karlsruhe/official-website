2e86f9ee4f30065331fd24f28afc12c8d59791e5
Daniel Meiborg <mail@danielmeiborg.com>
2025-09-20 00:18:14 +0200

    add draft  landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Index Page</div>
+    <div>
+      <div className="absolute top-0 left-0">
+        <div className="flex flex-col items-center justify-center px-10 w-screen min-h-screen bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)]">
+          <div className="flex flex-col 2xl:w-[160ch] space-y-[20vh] my-[20vh]">
+            <h1 className="text-9xl font-bold text-left">BEST Karlsruhe</h1>
+            <h1 className="text-7xl font-bold text-right">Something cool</h1>
+            <h1 className="text-7xl font-bold text-left">Something else cool</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }

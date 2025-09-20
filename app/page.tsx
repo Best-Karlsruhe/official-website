c8de2db10c02c5a3c6451f1032439cfb335b9a04
Daniel Meiborg <mail@danielmeiborg.com>
2025-09-20 02:49:15 +0200

    use inset-0 for gradient

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@
 export default function Home() {
   return (
     <div>
-      <div className="absolute top-0 left-0">
-        <div className="w-screen h-screen flex flex-col items-center bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)]">
-          <div className="flex flex-col justify-evenly px-10 h-screen 2xl:w-[150ch] w-full">
-            <h1 className="2xl:text-8xl text-6xl font-bold text-left self-start-safe">OG BEST Karlsruhe</h1>
-            <h1 className="2xl:text-6xl text-5xl font-bold text-right self-end-safe">Something cool</h1>
-            <h1 className="2xl:text-6xl text-5xl font-bold text-left self-start-safe">Something else cool</h1>
-          </div>
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0 w-full h-full bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)] z-0"></div>
+
+        <div className="relative flex flex-col justify-evenly px-10 h-screen 2xl:w-[150ch] w-full z-10">
+          <h1 className="2xl:text-8xl text-6xl font-bold text-left self-start-safe">OG BEST Karlsruhe</h1>
+          <h1 className="2xl:text-6xl text-5xl font-bold text-right self-end-safe">Something cool</h1>
+          <h1 className="2xl:text-6xl text-5xl font-bold text-left self-start-safe">Something else cool</h1>
         </div>
       </div>
+
     </div>
   );
 }

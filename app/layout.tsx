dc74cec77de3472725815530012a142cd2fce098
Daniel Meiborg <mail@danielmeiborg.com>
2025-09-20 03:12:44 +0200

    with viewport-fit=cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </head>
       <body>
         <NavBar />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-'use client'
-
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [vh, setVh] = useState(0);
-
-  useEffect(() => {
-    const updateVh = () => setVh(window.innerHeight);
-    updateVh();
-    window.addEventListener("resize", updateVh);
-    return () => window.removeEventListener("resize", updateVh);
-  }, []);
-
   return (
     <div>
-      <div style={{ height: vh }} className="relative w-full overflow-hidden">
+      <div className="h-screen w-full">
         <div className="absolute inset-0 w-full h-full bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)] z-0"></div>
 
         <div className="relative px-10 w-full h-full z-10 flex flex-col items-center">

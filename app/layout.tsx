5d3d29e42401a5d10cb9e88e5c6833cce46a311a
Daniel Meiborg <mail@danielmeiborg.com>
2025-09-20 14:41:08 +0200

    bg red 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-red bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)]">
+    <html lang="en" className="bg-red-700">
+      <body className="bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)]">
         <NavBar />
         {children}
       </body>

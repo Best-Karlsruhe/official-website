7796eb805f18d2cc33a8f6e21307e7e3e50a5b60
Daniel Meiborg <mail@danielmeiborg.com>
2025-09-20 14:30:48 +0200

    s

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <div className="h-[100dvh] w-full bg-cover">
-        {/* <div className="bg-cover absolute inset-0 w-full h-[100dvh] bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)] z-0"></div> */}
+        {/* <div className="backdrop absolute inset-0 w-full h-[100dvh] bg-[linear-gradient(257deg,hsla(18,84%,90%,0.35)_-32.4%,rgba(255,165,129,0.35)_-3.42%,rgba(255,73,0,0.35)_23.87%,rgba(255,140,160,0.35)_50.46%,rgba(255,120,170,0.35)_79.15%,rgba(249,57,166,0.35)_103.64%)] z-0"></div> */}
 
         <div className="relative px-10 w-full h-[100dvh] z-10 flex flex-col items-center">
           <div className="flex flex-col justify-evenly w-full h-full max-w-[150ch]">
